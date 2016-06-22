78c46730018e42a39214e5ab406d351c1ba384b4
Rik van der Werf <rikvanderwerf@outlook.com>
2016-06-22 20:40:08 +0200

    test for good luck

--- smartgymapi/tests/music_preference.py
+++ smartgymapi/tests/music_preference.py
@@ -3,7 +3,37 @@ import uuid
 
 from pyramid import testing
 
-from smartgymapi.tests import UnitTestCase
+from smartgymapi.tests import UnitTestCase, FunctionalTestCase
+
+
+class FunctionalMusicPreferenceTest(FunctionalTestCase):
+
+    def test_post_music_preference_succesful(self):
+        from smartgymapi.models.user import User
+
+        salt = '$2b$12$X2xgb/JItJpDL7RKfZhqwu'
+        hashed_password = '$2b$12$X2xgb/JItJpDL7RKfZhqwubNVnj4onQS'\
+            'Qio8ECMHzXjizx4gqn1Rq'
+        user_id = uuid.uuid4()
+        user = User(id=user_id,
+                    first_name='test',
+                    last_name='person',
+                    password_hash=hashed_password,
+                    password_salt=salt,
+                    email='test@test.com',
+                    country='The Netherlands',
+                    date_of_birth=datetime.datetime.now())
+        self.session.add(user)
+        self.session.flush()
+
+        response = self.app.post_json(
+            '/music_preference',
+            {
+                'user_id': str(user_id),
+                'genre': 'dance',
+            })
+
+        self.assertEqual(response.status_code, 201)
 
 
 class UnitMusicPreferenceTest(UnitTestCase):
