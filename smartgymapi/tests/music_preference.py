de00897a1092e7e176379e1d2c304530bc18aac3
Rik van der Werf <rikvanderwerf@outlook.com>
2016-06-22 18:34:29 +0200

    removed test

--- smartgymapi/tests/music_preference.py
+++ smartgymapi/tests/music_preference.py
@@ -3,32 +3,7 @@ import uuid
 
 from pyramid import testing
 
-from smartgymapi.tests import UnitTestCase, FunctionalTestCase
-
-
-class FunctionalMusicPreferenceTest(FunctionalTestCase):
-
-    def test_list_music_preference_unsuccesful(self):
-        from smartgymapi.models.user import User
-
-        salt = '$2b$12$X2xgb/JItJpDL7RKfZhqwu'
-        hashed_password = '$2b$12$X2xgb/JItJpDL7RKfZhqwubNVnj4onQS'\
-            'Qio8ECMHzXjizx4gqn1Rq'
-        user_id = uuid.uuid4()
-        user = User(id=user_id,
-                    first_name='test',
-                    last_name='person',
-                    password_hash=hashed_password,
-                    password_salt=salt,
-                    email='test@test.com',
-                    country='The Netherlands',
-                    date_of_birth=datetime.datetime.now())
-        self.session.add(user)
-        self.session.flush()
-
-        response = self.app.get('/music_preference')
-
-        self.assertEqual(response.status_code, 401)
+from smartgymapi.tests import UnitTestCase
 
 
 class UnitMusicPreferenceTest(UnitTestCase):
